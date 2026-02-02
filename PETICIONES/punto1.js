931e2a77ef5e3e89e02e5fd3f3c2a59a336128bd
DIEGO-PUERTO <puerto7171@hotmail.com>
2026-02-02 16:47:05 -0500

    Estructura inicial del proyecto API y configuración de Node

---
+++ PETICIONES/punto1.js
@@ -0,0 +1,30 @@
+//Solicitud 1: Realice una solicitud GETpara obtener la lista completa de usuarios disponibles
+//en el servicio.
+
+const 
+
+
+
+
+
+
+
+
+
+
+
+
+//Solicitud 2: Realice una solicitud GET para consultar la información de un usuario
+//específico, utilizando su identificador.
+
+
+
+//Solicitud 3: Realice una solicitud GET para obtener todas las publicaciones (posts)
+//asociadas a un usuario determinado.
+
+
+
+
+
+
+
